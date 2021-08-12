f389e729ba2832ef784843f45d02ead275ed3d2e
Dipen Boro <borod9200@gmail.com>
2021-08-12 13:41:20 +0530

    course section add

---
+++ app/course/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Text, View } from "react-native"
+
+export function CourseSection() {
+    return (
+        <View>
+            <Text>
+                This is course section
+            </Text>
+        </View>
+    )
+}
