b684acbd852ba1b0ea7a8c3ad2001922e364d73a
Dipen Boro <borod9200@gmail.com>
2026-07-31 16:06:26 +0530

    wrong color hook import fix

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { Colors } from '@/constants/theme';
 import { HapticTab } from '@/src/components/haptic-tab';
 import { IconSymbol } from '@/src/components/ui/icon-symbol';
-import { useColorScheme } from '@/src/hooks/use-color-scheme.web';
+import { useColorScheme } from '@/src/hooks/use-color-scheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
