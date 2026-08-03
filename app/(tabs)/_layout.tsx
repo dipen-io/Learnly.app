67b408c22c3dff8f75ca603120d582be1952f2e1
Dipen Boro <borod9200@gmail.com>
2026-08-03 15:28:06 +0530

    fix tabs/layout for all tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,8 +5,11 @@ import { Colors } from '@/constants/theme';
 import { HapticTab } from '@/src/components/haptic-tab';
 import { IconSymbol } from '@/src/components/ui/icon-symbol';
 import { useColorScheme } from '@/src/hooks/use-color-scheme';
+import { useAuthStore } from '@/src/store/auth-store';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
+  const isAuthenticated = useAuthStore((s) => s.isAuthenticated);
   const colorScheme = useColorScheme();
 
   return (
@@ -16,6 +19,7 @@ export default function TabLayout() {
         headerShown: false,
         tabBarButton: HapticTab,
       }}>
+
       <Tabs.Screen
         name="index"
         options={{
@@ -23,19 +27,40 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
+
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search-outline" color={color} size={size} />
+          ),
         }}
       />
+
+      <Tabs.Screen
+        name='my-learning'
+        options={{
+          title: 'My Learning',
+          href: isAuthenticated ? undefined : null,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="play-circle-outline" color={color} size={size} />
+          )
+        }}
+
+      />
+
       <Tabs.Screen
         name="profile"
         options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="profile" color={color} />,
-
+          title: isAuthenticated ? 'Profile' : 'Account',
+          tabBarIcon: ({ color, size }) =>
+            // <IconSymbol size={28} name="profile" color={color} />,
+            <Ionicons
+              name={isAuthenticated ? 'person-circle-outline' : 'person-outline'}
+              color={color}
+              size={size}
+            />
         }}
       />
     </Tabs>
