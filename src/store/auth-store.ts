2bd8b430e352dfdc599301f9fd347f7989ee4cfb
Dipen Boro <borod9200@gmail.com>
2026-08-03 12:01:04 +0530

    updated auth-store for adding clearssion and other

--- src/store/auth-store.ts
+++ src/store/auth-store.ts
@@ -10,12 +10,12 @@ import { create } from "zustand";
 import { authApi } from "../api/auth.api";
 import { apiClient } from "../api/client";
 import type { User } from "../types/user";
-import { deleteToken, getToken, saveToken } from "../utils/token-storage";
+import { deleteTokens, getAccessToken, saveToken } from "../utils/token-storage";
 
 
 type AuthState = {
     user: User | null;
-    token: string | null;
+    // token: string | null,
     isAuthenticated: boolean,
     isHydrated: boolean, // true once we've checked secure-store on app launce
 
@@ -23,19 +23,21 @@ type AuthState = {
     login: (email: string, password: string) => Promise<void>;
     signup: (name: string, email: string, password: string) => Promise<void>;
     logout: () => Promise<void>;
+    //called internally by client.ts 's interceptor when a refresh is fialed
+    // refresh token expired/invalid 
+    clearSession: () => void;
 }
 
 export const useAuthStore = create<AuthState>((set, get) => ({
     user: null,
-    token: null,
     isAuthenticated: false,
     isHydrated: false,
 
     hydrate: async () => {
-        const token = await getToken();
-        if (token) {
-            apiClient.defaults.headers.common.Authorization = `Bearer ${token}`;
-            set({ token, isAuthenticated: true, isHydrated: true });
+        const accessToken = await getAccessToken();
+        if (accessToken) {
+            apiClient.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+            set({ isAuthenticated: true, isHydrated: true });
             //TODO: later will do this 
             // Optionally verify + refresh user in background:
             // authApi.me().then((user) => set({ user }));
@@ -46,27 +48,32 @@ export const useAuthStore = create<AuthState>((set, get) => ({
 
 
     login: async (email, password) => {
-        const { token, user } = await authApi.login(email, password);
+        const { accessToken, refreshToken, user } = await authApi.login(email, password);
 
-        await saveToken(token);
-        apiClient.defaults.headers.common.Authorization = `Bearer ${token}`;
-        set({ token, user, isAuthenticated: true });
+        await saveToken(accessToken, refreshToken);
+        apiClient.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+        set({ user, isAuthenticated: true });
     },
 
     signup: async (name, email, password) => {
-        const { token, user } = await authApi.signup(name,
+        const { accessToken, refreshToken, user } = await authApi.signup(name,
             email,
             password
         );
 
-        await saveToken(token);
-        apiClient.defaults.headers.common.Authorization = `Bearer ${token}`;
-        set({ token, user, isAuthenticated: true });
+        await saveToken(accessToken, refreshToken);
+        apiClient.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+        set({ user, isAuthenticated: true });
     },
 
     logout: async () => {
-        await deleteToken();
+        await deleteTokens();
         delete apiClient.defaults.headers.common.Authorization;
-        set({ token: null, user: null, isAuthenticated: false });
+        set({ user: null, isAuthenticated: false });
+    },
+
+    clearSession() {
+        delete apiClient.defaults.headers.common.Authorization;
+        set({ user: null, isAuthenticated: false });
     },
 }));
