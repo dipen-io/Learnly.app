21b96a65025152aaee73f87c16007dc1b84c4c31
Dipen Boro <borod9200@gmail.com>
2026-08-03 13:08:30 +0530

    add global query kyes

---
+++ src/api/query-keys.ts
@@ -0,0 +1,18 @@
+//src/api/query-keys.ts
+
+export const queryKeys = {
+    courses: {
+        all: ['courses'] as const,
+        list: (params?: { category?: string; search?: string }) =>
+            ['courses', 'list', params] as const,
+        detail: (courseId: string) => ['courses', 'detail', courseId] as const
+    },
+
+    myLearning: {
+        all: ['my-learning'] as const,
+    },
+
+    cart: {
+        all: ['card'] as const,
+    },
+};
