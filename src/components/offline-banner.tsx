9a262ed6e40b2494d9be216add80070c90ab48f6
Dipen Boro <borod9200@gmail.com>
2026-08-05 16:59:40 +0530

    add comp for offline-banner

---
+++ src/components/offline-banner.tsx
@@ -0,0 +1,22 @@
+//src/components/offline-banner.tsx
+
+import React from "react";
+import { View } from "react-native";
+import { useNetworkStatus } from "../hooks/use-network-status";
+import { ThemedText } from "./themed-text";
+
+function OfflineBanner() {
+    const { isOffline } = useNetworkStatus();
+
+    if (!isOffline) {
+        return null;
+    }
+
+    return (
+        <View>
+            <ThemedText>
+                No internet connection
+            </ThemedText>
+        </View>
+    );
+}
