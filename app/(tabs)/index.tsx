b90255072b44d9db7e553820931c47fb2733abc4
Dipen Boro <borod9200@gmail.com>
2026-08-06 23:46:26 +0530

    refresh react query spinner loading added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,5 @@
+import { useTheme } from '@/constants/theme';
+import { ThemeRefreshControl } from '@/src/components/themed-refresh-control';
 import { BannerCarousel } from '@/src/features/home/banner-carousel';
 import { CategoryPillList } from '@/src/features/home/category-pill-list';
 import { ContinueLearning } from '@/src/features/home/continue-learning';
@@ -5,15 +7,58 @@ import { FeaturedCourses } from '@/src/features/home/featured-courses';
 import { HomeHeader } from '@/src/features/home/home-header';
 import { RecommendedCourses } from '@/src/features/home/recommended-courses';
 import { TrendingCourses } from '@/src/features/home/trending-courses';
-import React from 'react';
+import { useQueryClient } from '@tanstack/react-query';
+import React, { useCallback, useState } from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
+  const queryClient = useQueryClient();
+  const { colors } = useTheme();
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+
+    await queryClient.invalidateQueries({
+      queryKey: ['banners'],
+      exact: false,
+    });
+    await queryClient.invalidateQueries({
+      queryKey: ['progress'],
+      exact: false,
+    });
+    await queryClient.invalidateQueries({
+      queryKey: ['featured'],
+      exact: false,
+    });
+    await queryClient.invalidateQueries({
+      queryKey: ['recommended'],
+      exact: false,
+    });
+    await queryClient.invalidateQueries({
+      queryKey: ['trending'],
+      exact: false,
+    });
+    await new Promise((r) => setTimeout(r, 600));
+
+    setRefreshing(false);
+  }, [queryClient])
 
   return (
     <ScrollView style={[styles.container]}
       contentContainerStyle={{ paddingTop: 20, paddingBottom: 24 }}
-      showsVerticalScrollIndicator={false}>
+      showsVerticalScrollIndicator={false}
+      refreshControl={
+        <ThemeRefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+        />
+        // <FancyRefreshControl
+        //   refreshing={refreshing}
+        //   onRefresh={onRefresh}
+        // />
+      }
+    >
 
       {/* 1. Header Section */}
       <HomeHeader />
