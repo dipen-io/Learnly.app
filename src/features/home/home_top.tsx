0c82f6c702e398185c25fca016160271e0bcd461
Dipen Boro <borod9200@gmail.com>
2026-08-16 10:40:59 +0530

    add an home top file

---
+++ src/features/home/home_top.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { View } from "react-native";
+import { BannerCarousel } from "./banner-carousel";
+import { useBanners } from "./use-home-sections";
+
+export function Home_Top() {
+    const { data: banners, isError, isLoading } = useBanners();
+
+    if (isLoading || isError || !banners) return null;
+
+    const homeTopBanners = banners
+        .filter((b) => b.position === 'home_top')
+        .sort((a, b) => a.priority - b.priority);
+
+    return (
+        <View>
+            {homeTopBanners.map((banner) => {
+                if (banner.type === 'hero_carousel') {
+                    return <BannerCarousel key={banner.id} banner={banner} />;
+                }
+                if (banner.type === 'promo_strip') {
+                    // return <PromoStrip key={banner.id} content={banner.content} />;
+                    return null;
+                }
+                if (banner.type === 'announcement') {
+                    // return <AnnouncementBar key={banner.id} content={banner.content} />;
+                    return null;
+                }
+                return null;
+            })}
+        </View>
+    );
+}
